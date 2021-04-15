50adc51780ff0edc988834f15b5745071bea226c
GobinathAL <58905033+GobinathAL@users.noreply.github.com>
2021-04-15 13:22:44 +0530

    Add files via upload

--- ExceptionHandling/Hello.java
+++ ExceptionHandling/Hello.java
@@ -2,35 +2,48 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Hello {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter how many elements in an array");
         int n = sc.nextInt();
         int[] a = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int num;
             System.out.println("Enter an odd number between 1 and 100 inclusive");
-            while(true) {
+            while (true) {
                 try {
                     num = sc.nextInt();
-                    if(num < 1 || num > 100) {
+                    if (num < 1 || num > 100) {
                         throw new Exception("NumberOutOfRange");
-                    } else if(num % 2 == 0) {
+                    } else if (num % 2 == 0) {
                         throw new Exception("EvenNumberEntered");
-                    } else break;
-                } catch(Exception e) {
-                    if(e.toString().contains("NumberOutOfRange")) {
+                    } else
+                        break;
+                } catch (Exception e) {
+                    if (e.toString().contains("NumberOutOfRange")) {
                         System.out.println("please enter number in the range 1 to 100 inclusive");
-                    } else if(e.toString().contains("EvenNumberEntered")) {
+                    } else if (e.toString().contains("EvenNumberEntered")) {
                         System.out.println("please enter an odd number");
                     }
-                    
-                } 
+
+                }
             }
             a[i] = num;
-       }
-       for(int i = 0; i < n; i++) {
-           System.out.println(a[i]);
-       }
+        }
+        System.out.println("Enter the index to get the element");
+        while (true) {
+            int index = sc.nextInt();
+            try {
+                System.out.println(a[index]);
+                return;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Index out of range");
+            } finally {
+                if (index >= a.length || index < 0)
+                    System.out.println("Enter again");
+                else
+                    System.out.println("Element retrieved");
+            }
+        }
     }
 }
